4a615d047b61ba4315af5c97ec5cfa4176e0de20
Tang wei xun <tangweixun@Tangs-Mac-mini.local>
2018-11-02 06:21:28 +0800

    Project Update

---
+++ iAuth/iAuth/TestOut.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace iAuth
+{
+    public partial class TestOut : ContentPage
+    {
+        public TestOut()
+        {
+            InitializeComponent();
+        }
+
+
+        void Handle_Click (object sender, System.EventArgs e){
+            DisplayAlert("Title", "fml","cancel");
+        }
+    }
+}
