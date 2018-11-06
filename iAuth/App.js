b68ea47009acdecbec2a5f35d4de12bde2864f8e
Tang wei xun <tangweixun@Tangs-Mac-mini.local>
2018-11-06 18:23:40 +0800

    yarn install & react navigation lib added

--- iAuth/App.js
+++ iAuth/App.js
@@ -7,21 +7,30 @@
  */
 
  import React from 'react';
- import { View, Text } from 'react-native';
- import { createStackNavigator } from 'react-navigation';
+ import { Text, View } from 'react-native';
+ import { createBottomTabNavigator } from 'react-navigation';
 
  class HomeScreen extends React.Component {
    render() {
      return (
-       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-         <Text>Home Screen</Text>
+       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Text>Home!</Text>
        </View>
      );
    }
  }
 
- export default createStackNavigator({
-   Home: {
-     screen: HomeScreen
-   },
+ class SettingsScreen extends React.Component {
+   render() {
+     return (
+       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+         <Text>Settings!</Text>
+       </View>
+     );
+   }
+ }
+
+ export default createBottomTabNavigator({
+   Home: HomeScreen,
+   Settings: SettingsScreen,
  });
