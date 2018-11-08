c8fb5324cbd974a9d365768d1efc66e355b1d3a3
Tang wei xun <tangweixun@Tangs-Mac-mini.local>
2018-11-09 01:34:37 +0800

    props * state js added

--- iAuth/index.js
+++ iAuth/index.js
@@ -2,6 +2,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import HelloWorld from './HelloWorld';
+import Props from './Props';
 import {name as appName} from './app.json';
+
 
 AppRegistry.registerComponent(appName, () => App);
