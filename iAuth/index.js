1b5b2994a8d50940d7637f5216f3b71623f953ac
Tang wei xun <tangweixun@Tangs-Mac-mini.local>
2018-11-10 17:26:36 +0800

    QR Scanner Tested Can Scan

--- iAuth/index.js
+++ iAuth/index.js
@@ -5,7 +5,21 @@ import App from './App';
 import HelloWorld from './HelloWorld';
 import Props from './Props';
 import {name as appName} from './app.json';
-//https://www.techiediaries.com/reactnative-android-barcode-scanner/
-import QRScanner from './QRScanner';
+
+import QRScanner from './src/component/QRScanner';
 
 AppRegistry.registerComponent(appName, () => QRScanner);
+
+
+
+
+
+
+/**
+ * 
+ * QR_Camera Notes:-
+ * 
+ * - React-Navigation lib Needed. 
+ * - Camera Permission.
+ * 
+ */
