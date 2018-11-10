bba1208147c3801ef220f96254574c02155448d5
Tang wei xun <tangweixun@Tangs-Mac-mini.local>
2018-11-10 17:13:42 +0800

    QR Scanner Tested Can Scan

--- iAuth/App.js
+++ iAuth/App.js
@@ -14,7 +14,7 @@
    render() {
      return (
        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-         <Text>Home!</Text>
+         <Text>Home</Text>
        </View>
      );
    }
--- iAuth/index.js
+++ iAuth/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import HelloWorld from './HelloWorld';
 import Props from './Props';
 import {name as appName} from './app.json';
+//https://www.techiediaries.com/reactnative-android-barcode-scanner/
+import QRScanner from './QRScanner';
 
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => QRScanner);
